b888a17672ec395553536222e8eb9a6ea63c9223
shatab <shatabag4749@gmail.com>
2024-02-07 21:45:15 +0600

    Updated cv

--- src/Components/SeeResume.jsx
+++ src/Components/SeeResume.jsx
@@ -1,14 +1,29 @@
 
-import cvPic from '../assets/Cv/cv-pic.jpg'
+import { Document, Page } from 'react-pdf';
+import cvPDF from '../assets/Cv/resume.pdf'
+import { useState } from 'react';
 
 
 const SeeResume = () => {
+    const [numPages, setNumPages]= useState()
+    const [pageNumber, setPageNumber]= useState(1)
+
+    const handleSuccess = ({numPages})=>{
+        setNumPages(numPages)
+    }
 
     return (
         <div>
             <dialog id="my_modal_5" className="modal">
                 <div className="modal-box w-11/12 max-w-5xl">
-                    <img src={cvPic} alt="" />
+                    <div>
+                        <Document
+                        file={cvPDF} 
+                        onLoadSuccess={handleSuccess}
+                        >
+                            <Page pageNumber={pageNumber}/>
+                        </Document>
+                    </div>
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button, it will close the modal */}
