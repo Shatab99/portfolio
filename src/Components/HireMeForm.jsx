d70e0940353edc696ea455932a0856181bd44a1e
shatab <shatabag4749@gmail.com>
2024-09-13 16:52:09 +0600

    reactivate email service

--- src/Components/HireMeForm.jsx
+++ src/Components/HireMeForm.jsx
@@ -30,7 +30,7 @@ const HireMeForm = () => {
                 <div className="modal-box">
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <button onClick={()=>setDisplay(true)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
                     {
                         display ?
